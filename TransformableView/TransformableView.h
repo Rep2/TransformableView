44374f246f3d6e172d42de5bffcdb5f5365598f8
Ivan Rep <ivan.rep2@gmail.com>
2017-12-10 02:30:14 +0100

    Initial commit

---
+++ TransformableView/TransformableView.h
@@ -0,0 +1,19 @@
+//
+//  TransformableView.h
+//  TransformableView
+//
+//  Created by Ivan Rep on 10/12/2017.
+//  Copyright © 2017 Rep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TransformableView.
+FOUNDATION_EXPORT double TransformableViewVersionNumber;
+
+//! Project version string for TransformableView.
+FOUNDATION_EXPORT const unsigned char TransformableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransformableView/PublicHeader.h>
+
+
